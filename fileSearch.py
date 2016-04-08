7400f1c88af07561ee7d9a8c8d2284f2b82c486c
Tanner <tparker12@mail.wou.edu>
2016-04-08 00:10:34 -0700

    added content the the README.txt including a simple copyright, as well as an introduction and how to 'install'.

--- fileSearch.py
+++ fileSearch.py
@@ -9,7 +9,11 @@ import sys
 import os
 import shutil
 
-#method for copying a file with shutil
+#imports for dialogue boxes
+import Tkinter
+import tkFileDialog
+
+# method for copying a file with shutil
 def copyFile(src, dest):
     try:
         shutil.copy2(src, dest)
@@ -20,6 +24,15 @@ def copyFile(src, dest):
     except IOError as e:
         print('Error: %s' % e.strerror)
 
+# method to call a gui to select a directory
+# found at http://stackoverflow.com/questions/25282883/how-can-i-use-the-output-from-tkfiledialog-askdirectory-to-fill-a-tkinter-entry
+def askdirectory():
+    root = Tkinter.Tk()
+    # withdraw() allows the window to dissapear 
+    # found at https://www.daniweb.com/programming/software-development/threads/210657/open-directory-dialog-box
+    root.withdraw()
+    dirname = tkFileDialog.askdirectory()
+    return dirname
 
 # /home/tanner/Desktop/firstFolder
 
@@ -27,32 +40,33 @@ print "This script is meant for scrubbing files of a specified type and listing 
 print "It also allows you to copy the files after collecting them into a folder of your choice."
 
 print "Please enter a starting directory.."
-directory = raw_input()
+directory = askdirectory()
+#directory = raw_input()
 
-print 'If you want all the image files then press 1'
-print 'If you want all the Word files then press 2'
-print 'If you want all the  video files then press 3'
-print 'If you want all audio files then press 4'
-print 'If you want all .jpg files then press 5'
-print 'If you want all .txt files then press 6'
+print 'For image files then press 1'
+print 'For Word files then press 2'
+print 'For video files then press 3'
+print 'For audio files then press 4'
+print 'For .jpg files then press 5'
+print 'For .txt files then press 6'
 choice = int(raw_input('Please enter your choice: '))
 
-#variables to hold custom paths...this is meant to be used later on.
+# variables to hold custom paths...this is meant to be used later on.
 desktop = '/home/tanner/Desktop/'
 school = '/home/tanner/Desktop/School'
 pictures = '/home/tanner/Pictures'
 videos = '/home/tanner/Videos'
 
-#make tuples to hold file extensions
+# make tuples to hold file extensions
 imageExtensions = ('.jpg', '.png', '.bmp', '.gif')
-#tuple to hold word doc extensions
+# tuple to hold word doc extensions
 wordExtensions = ('.odt', '.doc', '.docx')
-#tuple for video extensions
+# tuple for video extensions
 videoExtensions = ('.mov', '.MOV', '.mpeg', '.mp4', '.avi', '.mkv')
-#tuple for audio extensions
+# tuple for audio extensions
 audioExtensions = ('.wav', '.mp3', '.ogg')
 
-#create a list to store successfull matches
+# create a list to store successfull matches
 matches = []
 
 for root, dirnames, filenames in os.walk(directory):
@@ -78,7 +92,7 @@ for root, dirnames, filenames in os.walk(directory):
         else:
             print 'You did not press a specified number.'
             sys.exit()
-#if the list is empty say so..
+# if the list is empty say so..these are new changes.
 if len(matches) == 0:
     print len(matches)
     print 'There were no files of the specified type within the directory you gave.'
@@ -88,17 +102,18 @@ else:
     answer = raw_input()
     if answer == 'y' or 'Y':
         print 'Specify directory path to move files into.'
-        print 'Note: The folder you specify will be created on the Desktop.'
-        newDir = raw_input()
-        #concatenates the specified folder with the path to the desktop
-        newDir1 = desktop + newDir
-        #checks that the directory doesn't already exist, if not; makes it.
-        if not os.path.exists(newDir1):
-            os.makedirs(newDir1)
-        #loops through the list of matches and copy's them to the new folder.
+        direc = askdirectory()
+        #print 'Note: The folder you specify will be created on the Desktop.'
+        #newDir = raw_input()
+        # concatenates the specified folder with the path to the desktop
+        #newDir1 = desktop + newDir
+        # checks that the directory doesn't already exist, if not; makes it.
+        if not os.path.exists(direc):
+            os.makedirs(direc)
+        # loops through the list of matches and copy's them to the new folder.
         for x in matches:
-            copyFile(x, newDir1)
-        print "Congrats, you moved all the files into %s" % newDir1
+            copyFile(x, direc)
+        print "Congrats, you moved all the files into %s" % direc
         sys.exit()
     else:
         print "You have chosen not to move the files to a new folder, goodbye."
